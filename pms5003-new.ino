0867306dee555578286c30b68a6fcb4d5c7c591a
Pak Kei <106027537+Pakkei0920@users.noreply.github.com>
2023-07-02 16:00:12 +0800

    Update pms5003-new.ino

--- pms5003-new.ino
+++ pms5003-new.ino
@@ -13,15 +13,13 @@ void loop()
 {
   if (pms.read(data))
   {
-    Serial.print("PM 1.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_1_0);
-
-    Serial.print("PM 2.5 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_2_5);
-
-    Serial.print("PM 10.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_10_0);
-
+    int pm1 = data.PM_AE_UG_1_0;
+    int pm25 = data.PM_AE_UG_2_5;
+    int pm100 = data.PM_AE_UG_10_0;
+    
+    Serial.print("PM 1.0(ug/m3): ");Serial.println(pm1);
+    Serial.print("PM 2.5(ug/m3): ");Serial.println(pm25);
+    Serial.print("PM 10.0(ug/m3): ");Serial.println(pm100);
     Serial.println();
   }
 }
